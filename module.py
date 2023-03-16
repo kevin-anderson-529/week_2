0ab91011975f121ecf4bd1dc922eae1395365b30
kevin-anderson-529 <126824343+kevin-anderson-529@users.noreply.github.com>
2023-03-15 23:02:00 -0400

    Add files via upload

---
+++ module.py
@@ -0,0 +1,10 @@
+import math
+
+def calc_square_footage(length, width):
+    return length * width
+
+def calc_circumference(radius):
+    return 2 * math.pi * radius
+
+def feet_to_inches(feet):
+    return feet * 12
